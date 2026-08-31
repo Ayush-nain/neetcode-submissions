db7a593d9d5ceaf5e79a8469e6eb16fa6a4d3e3a
Ayush-nain <ayushnainwal135@gmail.com>
2026-08-31 21:15:50 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) 
+    {
+       Stack<Character> st=new Stack<>();
+       if(s.length()==1)
+       {
+        return false;
+       }
+       for(char c:s.toCharArray())
+       {
+        if(c=='{' || c=='[' || c=='(')
+        {
+            st.push(c);
+        }
+        else if(st.isEmpty() || s.length()==1)
+        {
+            return false;
+        }
+        else
+        {
+            char ch=st.pop();
+            if((c=='}' && ch!='{') || (c==']' && ch!='[') || (c==')' && ch!='('))
+            {
+                return false;
+            }
+        }
+       }
+       if(!st.isEmpty())
+       {
+        return false;
+       }
+       return true;
+    }
+}
