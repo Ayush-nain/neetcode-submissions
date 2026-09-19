831e1f9305d748a7d151a49ae951f7a142416b53
Ayush-nain <ayushnainwal135@gmail.com>
2026-09-19 22:14:19 +0530

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) 
+    {
+        s=s.toLowerCase();
+        String st="";
+        for(int i=0;i<s.length();i++)  
+        {
+            if(Character.isLetterOrDigit(s.charAt(i)))
+            {
+                st=st+s.charAt(i);
+            }
+        }      
+        int l=0,r=st.length()-1;
+        while(l<=r)
+        {
+            if(st.charAt(l)!=st.charAt(r))
+            {
+                return false;
+            }
+            l++;r--;
+        }
+        return true;
+    }
+}
